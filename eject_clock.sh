9d030d612a030e2595cf10cd0812c5152799148a
horosora <horosora@gmail.com>
2017-09-10 00:03:47 +0900

    update

--- eject_clock.sh
+++ eject_clock.sh
@@ -4,10 +4,10 @@ while true
 do
   if [ $(date "+%M%S") = "0000" ]; then
     for i in `seq $(date "+%H")`
-      do
-        eject
-        eject -t
-      done
+    do
+      eject
+      eject -t
+    done
   fi
   sleep 1s
 done
